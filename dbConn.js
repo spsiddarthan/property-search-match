b5b8275f0720ed5b5a4fba0461fb3b0fd008d583
spsiddarthan <siddarthan@outlook.com>
2018-03-17 22:06:01 +0530

    Initi commit & migration script to create the properties table with the rows

---
+++ dbConn.js
@@ -0,0 +1,4 @@
+const Sequelize = require('sequelize');
+const config = require('./config');
+const sequelize = new Sequelize(`postgres://${config.dbUsername}:${config.dbPassword}@${config.dbHost}:${config.dbPort}/${config.dbName}`);
+module.exports = sequelize;
---
+++ models/property.js
@@ -0,0 +1,35 @@
+const dbConn = require('../dbConn.js');
+const Sequelize = require('sequelize');
+const Property = dbConn.define('Property',{
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        notNull: true,           
+    },
+    noofbathrooms: {
+        type: Sequelize.INTEGER,
+        notNull: true,    
+    },
+    noofbedrooms: {
+        type: Sequelize.INTEGER,
+        notNull: true,    
+    },
+    longitude: {
+        type: Sequelize.DOUBLE,
+        notNull: true,    
+    },
+    latitude: {
+        type: Sequelize.DOUBLE,
+        notNull: true,  
+    }
+
+},{
+    timestamps: false,
+  });
+//dbConn.sync({force: true});
+module.exports = Property;
+
